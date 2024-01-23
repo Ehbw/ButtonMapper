5b04bfcc1ea250a53a9342190190d96a8b904f35
Ehbw <ehbwwww@gmail.com>
2024-01-23 22:04:46 +0000

    Improvements
    From local branch

--- ClickMapper/src/utils/KeyManager.hpp
+++ ClickMapper/src/utils/KeyManager.hpp
@@ -1,89 +1,46 @@
 #pragma once
 #include <string>
-//Have no choice but to include the entirty of windows.h due to winnt.h not being self-contained.
+#include <unordered_map>
+
+#ifdef _WIN32
 #include <windows.h>
+#endif
+
 namespace ClickMapper {
-	
-	enum KeyType {
-		KEYBOARD = 0,
-		MOUSE = 1
+
+	static HHOOK m_hook;
+
+	class KeyManager {
+	public:
+		enum KeyType 
+		{
+			KEYBOARD,
+			MOUSE,
+			UNKNOWN
+		};
+	private:
+		KeyType GetKeyType(int VK);
+
+
+		std::unordered_map<int, int> m_registeredKeys;
+	public:
+
+		static KeyManager& Get();
+
+		static LRESULT CALLBACK HandleKeyPress(int nCode, WPARAM wParam, LPARAM lParam);
+		void HandleKey(HWND hwnd, int VK);
+
+		__fastcall KeyManager(HWND* currentProcess);
+		__fastcall ~KeyManager();
+
+		HWND* m_currentProcess;
+
+		void RegisterKey(int keyCode, int replacement);
+		void RemoveKey(int keyCode, int replacement);
+		const char* GetNameFromVK(int VK);
+		std::unordered_map<int, int>& GetKeys()
+		{
+			return m_registeredKeys;
+		}
 	};
-
-	KeyType GetKeyType(int VK) {
-		if ((VK >= (int)'A' && VK <= 'Z') || (VK >= 0x30 && VK <= 0x39)) {
-			return KeyType::KEYBOARD;
-		}
-		if (VK == 0x01 || VK == 0x02 || VK == 0x04) {
-			return KeyType::MOUSE;
-		}
-	}
-
-
-	std::string VKToKeyName(int VK) {
-		if ((VK >= (int)'A' && VK <= 'Z') || (VK >= 0x30 && VK <= 0x39)) {
-			return (char*)&VK;
-		}
-
-		std::string returnChar;
-		switch (VK) {
-		case 0x01:
-			returnChar = "Little Mouse Button";
-			break;
-		case 0x02:
-			returnChar = "Right Mouse Button";
-			break;
-		case 0x04:
-			returnChar = "Middle Mouse Button";
-			break;
-		}
-		return returnChar;
-	}
-
-	void HandleKey(HWND hwnd, int VK) {
-		INPUT input = { 0 };
-		switch (VK) {
-		case 0x01:
-		case 0x02:
-		case 0x04:
-			int dwFlag = 0;
-			int dwReleaseFlag = 0;
-			input.type = INPUT_MOUSE;
-			if (VK == 0x01) {
-				dwFlag = MOUSEEVENTF_LEFTDOWN;
-				dwReleaseFlag = MOUSEEVENTF_LEFTUP;
-			}
-
-			if (VK == 0x02) {
-				dwFlag == MOUSEEVENTF_RIGHTDOWN;
-				dwReleaseFlag = MOUSEEVENTF_RIGHTUP;
-			}
-
-			if (VK == 0x04) {
-				// Wrong event? MOUSEEVENTF_HWHEEL seems more reasonable, but lacks a release flag? Maybe Windows doesn' check for a release?.
-				dwFlag = MOUSEEVENTF_MIDDLEUP;
-				dwReleaseFlag = MOUSEEVENTF_MIDDLEDOWN;
-			}
-
-			input.mi.dwFlags = dwFlag;
-			SendInput(1, &input, sizeof(INPUT));
-			// Zero out existing data to prepare for release Flag.
-			::ZeroMemory(&input, sizeof(INPUT));
-			input.type = INPUT_MOUSE;
-			input.mi.dwFlags = dwReleaseFlag;
-			SendInput(1, &input, sizeof(INPUT));
-			return;
-		}
-		input.type = INPUT_KEYBOARD;
-		KEYBDINPUT kb = { 0 };
-		kb.wVk = VK;
-		input.ki = kb;
-		SendInput(1, &input, sizeof(INPUT));
-		// Zero out to for Key Up.
-		::ZeroMemory(&input, sizeof(INPUT));
-		input.type = INPUT_KEYBOARD;
-		kb.dwFlags = KEYEVENTF_KEYUP;
-		kb.wVk = VK;
-		input.ki = kb;
-		SendInput(1, &input, sizeof(INPUT));
-	}
 }
--- ClickMapper/src/utils/ProccessManager.hpp
+++ ClickMapper/src/utils/ProccessManager.hpp
@@ -4,65 +4,47 @@
 #include <string>
 #include <unordered_map>
 #include <windows.h>
+#include <iostream>
 #include <Psapi.h>
 namespace ClickMapper {
-	namespace Proccess {
-		BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam)
+	bool IsProcessVisible(HWND hWnd)
+	{
+		LONG lStyle = GetWindowLongPtrW(hWnd, GWL_STYLE);
+
+		if ((lStyle & WS_VISIBLE) && (lStyle & WS_SYSMENU) && IsWindowVisible(hWnd))
 		{
-			std::vector<std::pair<std::string, HWND>>* WindowTitles = (std::vector<std::pair<std::string, HWND>>*) lParam;
-			int length = GetWindowTextLengthA(hWnd);
-			std::string WindowTitle(length + 1, '\0');
-			length = GetWindowTextA(hWnd, &WindowTitle[0], length);
-			WindowTitle.resize(length);
-			WindowTitles->push_back(std::make_pair(WindowTitle, hWnd));
+			return true;
+		}
+
+		return false;
+	}
+
+	BOOL CALLBACK EnumWindowsProc(HWND hWnd, LPARAM lParam)
+	{
+		std::vector<std::pair<std::string, HWND>>* WindowTitles = (std::vector<std::pair<std::string, HWND>>*) lParam;
+		if (!IsProcessVisible(hWnd))
+		{
 			return TRUE;
 		}
 
-		std::vector<std::pair<std::string, HWND>> GetProcesses() {
-			std::vector<std::pair<std::string, HWND>> Windows;
-			if (EnumWindows(EnumWindowsProc, (LPARAM)&Windows)) {
-				return Windows;
-			}
+		int length = GetWindowTextLengthA(hWnd);
+		if (length != 0)
+		{
+			//GetWindowTextLengthA seems to return one less then the actual length of the text.
+			length += 1;
+			std::string WindowTitle(length, '\0');
+			length = GetWindowTextA(hWnd, &WindowTitle[0], length);
+			WindowTitle.resize(length);
+			WindowTitles->push_back(std::make_pair(WindowTitle, hWnd));
+		}
+		return TRUE;
+	}
+
+	std::vector<std::pair<std::string, HWND>> GetProcesses() {
+		std::vector<std::pair<std::string, HWND>> Windows;
+		if (EnumWindows(EnumWindowsProc, (LPARAM)&Windows)) {
 			return Windows;
 		}
-
-
-		/*std::vector<std::pair<std::string, int>> GetProcesses() {
-			DWORD Processes[1024], cbNeeded, cProcesses;
-			std::vector<std::pair<std::string, int>> _map;
-			if (!EnumProcesses(Processes, sizeof(Processes), &cbNeeded)) {
-				return _map;
-			}
-
-
-			cProcesses = cbNeeded / sizeof(DWORD);
-			
-			for (int i = 0; i < cProcesses; i++) {
-				if (Processes[i] != 0) {
-					DWORD processID = Processes[i];
-					TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
-					HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
-						PROCESS_VM_READ,
-						FALSE, processID);
-					if (NULL != hProcess) {
-						HMODULE hMod;
-						DWORD isCbNeeded;
-						if (EnumProcessModules(hProcess, &hMod, sizeof(hMod), &isCbNeeded)) {
-							GetModuleBaseName(hProcess, hMod, szProcessName, sizeof(szProcessName) / sizeof(TCHAR));
-						}
-#ifndef UNICODE
-						std::string _name;
-						_name = szProcessName;
-						_map.push_back(std::make_pair(_name, processID));
-#else
-						std::wstring wStr = szProcessName;
-						_map.push_back(std::make_pair(std::string(wStr.begin(), wStr.end()), processID));
-#endif
-						
-					}
-				}
-			}
-			return _map;
-		}*/
+		return Windows;
 	}
 }
